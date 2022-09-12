e4183f388129c7d776f1a595180721206dc0935b
Yatin <yatin.pandit@shorelineiot.com>
2022-09-12 09:23:17 +0530

    Removed unnecessary codes

--- internal_node.py
+++ internal_node.py
@@ -1,7 +1,6 @@
 def find_internal_nodes_num(tree):
     if not len(tree):
         return 0
-    sorted_tree = sorted(set(tree), reverse=True)
-    sorted_tree.pop()
-    return len(sorted_tree)
+    sorted_tree = set(tree)
+    return len(sorted_tree) - 1
 
